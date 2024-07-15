046c3a1f93f52d09c47b9519b479b5b2dc2eadc7
supersolid-nathan <58428517+supersolid-nathan@users.noreply.github.com>
2024-07-15 17:51:15 +0100

    updates index script

--- batch_layer_rename/index.js
+++ batch_layer_rename/index.js
@@ -2,48 +2,61 @@ function executePhotoshopModal(callback, commandName) {
   return window.require('photoshop').core.executeAsModal(callback, { commandName });
 }
 
+function getInputValue(selector) {
+  const selectorValue = document.querySelector(selector).value;
+  if (!selectorValue) {
+    console.log('No text entered');
+    return null;
+  }
+  return selectorValue;
+}
+
 function renameLayerNames() {
+  const newName = getInputValue('#rename_layer_text');
+  if (!newName) return;
+
   return executePhotoshopModal(
     () => {
       const app = window.require('photoshop').app;
 
       app.activeDocument.layers.forEach((layer) => {
-        layer.name = `${layer.name} (${layer.opacity} %)`;
+        layer.name = newName;
       });
-    },
-    {
-      commandName: 'Rename layers',
-    }
-  );
+    },'Rename layers');
 }
 
+
 function addPrefix() {
+  const prefixText = getInputValue('#add_prefix_text');
+  if (!prefixText) return;
+
   return executePhotoshopModal(
     () => {
       const app = window.require('photoshop').app;
-
-      const prefixText = getPrefixName();
 
       app.activeDocument.layers.forEach((layer) => {
         layer.name = `${prefixText}_${layer.name}`;
       });
-    },
-    {
-      commandName: 'Append Prefix',
-    }
-  );
+    }, 'Add Prefix');
 }
 
-function getPrefixName() {
-  const prefixText = document.querySelector('#add_prefix_text').value;
-  if (!prefixText) {
-    console.log('No text entered');
-    return;
-  }
-  return prefixText;
+function addSuffix() {
+  const suffixText = getInputValue('#add_suffix_text');
+  if (!suffixText) return;
+
+  return executePhotoshopModal(
+    () => {
+      const app = window.require('photoshop').app;
+
+      app.activeDocument.layers.forEach((layer) => {
+        layer.name = `${suffixText}_${layer.name}`;
+      });
+    },'Add Suffix');
 }
 
+// Adding event listeners to buttons
 document.getElementById('btnRename').addEventListener('click', renameLayerNames);
 document.getElementById('btnPrefix').addEventListener('click', addPrefix);
+document.getElementById('btnSuffix').addEventListener('click', addSuffix);
 
 //const regExp = /^(.*?)( \(\d+ %\))?$/;
