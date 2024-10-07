2204aa4797e82d15a2e791863667b139d837a818
supersolid-nathan <115369332+nrjones-dev@users.noreply.github.com>
2024-10-07 09:34:55 +0100

    code refactoring

--- psd_exporter/index.js
+++ psd_exporter/index.js
@@ -3,68 +3,59 @@ const { storage } = require('uxp');
 
 // Helper function to execute photoshop as modal.
 function executePhotoshopModal(callback, commandName) {
-  return require('photoshop').core.executeAsModal(callback, { commandName });
+  return window.require('photoshop').core.executeAsModal(callback, { commandName });
 }
 
-//
-// Flatten groups functionality
-//
-let mergeGroups = [];
+// Suffix name constants
 const SWIPE = '_SWIPE';
 const MERGE = '_MERGE';
 
+// find all relevant layers that are of kind GROUP and ends with SWIPE or MERGE constants
 function findLayers(layers) {
-  for (const layer of layers) {
-    if (layer.layers) findLayers(layer.layers);
-    let layer_name = layer.name;
-    console.log(constants);
-    if (layer.kind !== constants.LayerKind.GROUP) continue;
-    if (layer_name.endsWith(SWIPE) || layer_name.endsWith(MERGE)) {
+  const mergeGroups = [];
+  layers.forEach((layer) => {
+    if (layer.layers) {
+      mergeGroups.push(...findLayers(layer.layers));
+    }
+    if (layer.kind === constants.LayerKind.GROUP && (layer.name.endsWith(SWIPE) || layer.name.endsWith(MERGE))) {
       mergeGroups.push(layer);
     }
-  }
+  });
+  return mergeGroups;
 }
 
-function mergeLayers(mergeLayers) {
+// Remove suffix from gorups that will be merged, and merges them
+function mergeLayers(mergeGroups) {
   executePhotoshopModal(() => {
-    for (const layer of mergeLayers) {
-      if (layer.name.endsWith(SWIPE)) {
-        layer.name = layer.name.replace(SWIPE, '');
-      } else if (layer.name.endsWith(MERGE)) {
-        layer.name = layer.name.replace(MERGE, '');
-      }
+    mergeGroups.forEach((layer) => {
+      layer.name = layer.name.replace(SWIPE, '').replace(MERGE, '');
       layer.merge();
-    }
+    });
   }, 'merge layers');
 }
 
+// Main function that calls findLayers and mergeLayers on button press
 function flattenGroupsMain() {
   const documentLayers = app.activeDocument.layers;
-
-  findLayers(documentLayers);
+  const mergeGroups = findLayers(documentLayers);
 
   if (mergeGroups.length === 0) {
-    console.log('No layers to merge');
+    window.alert('No layers to merge');
     return;
   }
-
   mergeLayers(mergeGroups);
-  mergeGroups = [];
 }
 
-//
 // Resize and Export document as PSD functionality
-//
 async function resizeDocument() {
   await executePhotoshopModal(() => {
     app.activeDocument.resizeImage(1024, 1267);
-  }, 'resize doc');
+  }, 'resize document');
 }
 
 async function saveFile() {
-  const fileName = app.activeDocument.name;
-  const fileNameWithoutExt = fileName.replace(/\.[^/.]+$/, '');
-  let entry = await storage.localFileSystem.getFileForSaving(`${fileNameWithoutExt}_EXPORT.psd`);
+  const fileNameWithoutExt = app.activeDocument.name.replace(/\.[^/.]+$/, '');
+  const entry = await storage.localFileSystem.getFileForSaving(`${fileNameWithoutExt}_EXPORT.psd`);
   await executePhotoshopModal(() => {
     app.activeDocument.saveAs.psd(entry);
   }, 'save psd');
@@ -75,9 +66,6 @@ async function exportFile() {
   await saveFile();
 }
 
-//
 // Event listener calls
-//
-
 document.getElementById('flattenGroups').addEventListener('click', flattenGroupsMain);
 document.getElementById('exportFile').addEventListener('click', exportFile);
